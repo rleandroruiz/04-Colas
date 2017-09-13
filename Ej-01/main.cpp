0c15d306029ce76becbadfb22f90adb0fbe23205
rleandroruiz <leo1998.04.24@hotmail.com>
2017-09-13 12:02:20 -0300

    Avances en ejercicios 1 y 2

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include "../Cola/Cola.h"
+#include <string>
+using namespace std;
 
 int main() {
     std::cout << "Ejercicio 04/01\n" << std::endl;
+
+    char *g;
+
+
+    Cola<char> a, b;
+    int v = 1;
+
+    cin.getline(g);
+    cout << g << endl;
+
+    while(!a.esVacia() && !b.esVacia() && v == 1){
+        if(a.desencolar() != b.desencolar())
+            v = -1;
+    }
+
+    if(v == 1)
+        cout << "Son iguales" << endl;
+    else
+        cout << "NO son iguales" << endl;
+
+
+
     return 0;
 }
--- Ej-02/main.cpp
+++ Ej-02/main.cpp
@@ -1,7 +1,31 @@
+//TRAETE LA PILA!!!
+
 #include <iostream>
 #include "../Cola/Cola.h"
+#include <string>
+using namespace std;
 
 int main() {
     std::cout << "Ejercicio 04/02\n" << std::endl;
+
+
+    int v = 0;
+    string frase;
+    cin >> frase;
+    cout << frase << endl;
+
+    Cola<char> a;
+    Pila<char> b;
+
+    a.encolar();
+    //?????
+    b.push();
+
+    for(int i = 0; i <= frase.length(); i++ ){
+        if(a.desencolar() == b.pop())
+            v = 1;
+    }
+
     return 0;
 }
+
